5c34a720590157e4ec74e4fea3cb06b43db60487
Audrey Picarel <aud.picarel@gmail.com>
2025-10-23 13:45:54 +0200

    ajout header et footer + assets

---
+++ template_parts/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <ul> 
+        <li><a href="#mentionsLegales">MENTIONS LÉGALES/a></li>
+        <li><a href="#viePrivee">VIE PRIVÉE</a></li>
+        <li><a href="#droitsReserves">TOUS DROITS RÉSERVÉS</a></li>
+    </ul>
+</div>
---
+++ template_parts/header.php
@@ -0,0 +1,14 @@
+<div class="nav">
+    <div class="nav__logo">
+        <a href=" ">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/Logo.png" alt="Logo Mota Photo">
+        </a>
+    </div>
+    <div class="nav__menu">
+        <ul>
+            <li><a href="#accueil">ACCUEIL</a></li>
+            <li><a href="#about">À PROPOS</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+</div>
